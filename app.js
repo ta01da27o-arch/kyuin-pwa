e5ad2bc8f358dd818581614c07267b10f7cf16b3
ta01da27o-arch <ta01da27o@gmail.com>
2025-09-01 07:11:08 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById("app").innerHTML = `
+  <h2>サンプル出走表</h2>
+  <ul>
+    <li>1号艇：選手A - 逃げ</li>
+    <li>2号艇：選手B - 差し</li>
+    <li>3号艇：選手C - まくり</li>
+  </ul>
+  <p>予想コメント：1号艇中心に展開。</p>
+`;
